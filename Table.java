1f8fd81cd96a1b9cafddfa51311306882d33d074
Sujan Shetty <144237005+SujanShettyy@users.noreply.github.com>
2024-04-30 18:53:11 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a+" * 1 = "+1*a);
+        System.out.println(a+" * 2 = "+2*a);
+        System.out.println(a+" * 3 = "+3*a);
+        System.out.println(a+" * 4 = "+4*a);
+        System.out.println(a+" * 5 = "+5*a);
+        System.out.println(a+" * 6 = "+6*a);
+        System.out.println(a+" * 7 = "+7*a);
+        System.out.println(a+" * 8 = "+8*a);
+        System.out.println(a+" * 9 = "+9*a);
+        System.out.println(a+" * 10 = "+10*a);
+    }
+}
